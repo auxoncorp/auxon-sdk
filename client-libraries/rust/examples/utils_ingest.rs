12b70e6466f3e421a9b76b4d40b108f30cafa334
Russell Mull <russell@auxon.io>
2024-04-30 14:12:41 -0700

    Add utils/ingest example

---
+++ client-libraries/rust/examples/utils_ingest.rs
@@ -0,0 +1,28 @@
+// A basic example using the plugin_utils::ingest module
+
+#[derive(serde::Serialize, serde::Deserialize, Debug)]
+struct Config {
+    // This can be set in the config file, or with the environment variable "EXAMPLE_SETTING"
+    setting: Option<u32>,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    auxon_sdk::init_tracing!();
+    let cfg = auxon_sdk::plugin_utils::ingest::Config::<Config>::load("EXAMPLE_")?;
+    let mut client = cfg.connect_and_authenticate().await?;
+
+    let tl = auxon_sdk::api::TimelineId::allocate();
+    client.switch_timeline(tl).await?;
+    client
+        .send_timeline_attrs("tl", [("tl_attr", 1.into())])
+        .await?;
+
+    for i in 0..10 {
+        client
+            .send_event("ev", i as u128, [("ev_attr", "hello".into())])
+            .await?;
+    }
+
+    Ok(())
+}
