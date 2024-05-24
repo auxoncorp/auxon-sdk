5e49c9b5f8b496c394495df048940f940bbb7795
Russell Mull <russell@auxon.io>
2024-05-24 10:10:21 -0700

    Fix import errors

--- client-libraries/rust/src/plugin_utils/serde.rs
+++ client-libraries/rust/src/plugin_utils/serde.rs
@@ -26,57 +26,57 @@
 /// `envy` crate issue: https://github.com/softprops/envy/issues/26
 pub fn from_str<'de, D, T>(deserializer: D) -> Result<Option<T>, D::Error>
 where
-    D: de::Deserializer<'de>,
-    T: FromStr,
-    T::Err: Display,
+    D: serde::de::Deserializer<'de>,
+    T: std::str::FromStr,
+    T::Err: std::fmt::Display,
 {
-    deserializer.deserialize_any(StringifiedOptionAnyVisitor(PhantomData))
+    deserializer.deserialize_any(StringifiedOptionAnyVisitor(std::marker::PhantomData))
 }
 
-pub struct StringifiedOptionAnyVisitor<T>(PhantomData<T>);
+pub struct StringifiedOptionAnyVisitor<T>(std::marker::PhantomData<T>);
 
-impl<'de, T> de::Visitor<'de> for StringifiedOptionAnyVisitor<T>
+impl<'de, T> serde::de::Visitor<'de> for StringifiedOptionAnyVisitor<T>
 where
-    T: FromStr,
-    T::Err: Display,
+    T: std::str::FromStr,
+    T::Err: std::fmt::Display,
 {
     type Value = Option<T>;
 
-    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
         formatter.write_str("a string compatible with from_str")
     }
 
     fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
     where
-        E: de::Error,
+        E: serde::de::Error,
     {
         Ok(Some(T::from_str(v).map_err(E::custom)?))
     }
 
     fn visit_bool<E>(self, v: bool) -> Result<Self::Value, E>
     where
-        E: de::Error,
+        E: serde::de::Error,
     {
         self.visit_str(&v.to_string())
     }
 
     fn visit_i64<E>(self, v: i64) -> Result<Self::Value, E>
     where
-        E: de::Error,
+        E: serde::de::Error,
     {
         self.visit_str(&v.to_string())
     }
 
     fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E>
     where
-        E: de::Error,
+        E: serde::de::Error,
     {
         self.visit_str(&v.to_string())
     }
 
     fn visit_f64<E>(self, v: f64) -> Result<Self::Value, E>
     where
-        E: de::Error,
+        E: serde::de::Error,
     {
         self.visit_str(&v.to_string())
     }
