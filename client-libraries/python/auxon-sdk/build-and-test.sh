9165b59943838e443ad1d6c19525b233c108ccd2
Russell Mull <russell@auxon.io>
2024-06-17 14:37:31 -0700

    Install pytest in test script

--- client-libraries/python/auxon-sdk/build-and-test.sh
+++ client-libraries/python/auxon-sdk/build-and-test.sh
@@ -1,9 +1,8 @@
 set -ex
 
-pip install maturin
+pip install maturin pytest
 maturin build -i python3.8
 pip uninstall auxon_sdk -y
-pip --verbose debug
 
 pip install target/wheels/auxon_sdk-*.whl
 pytest
